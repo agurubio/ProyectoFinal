49bf7b475c0f73c283b35154a84783b605692c48
Agustin Rubio <agustin.rubio@gmail.com>
2022-02-09 15:59:25 -0300

    primeras vistas en app

--- Final/AppFinal/models.py
+++ Final/AppFinal/models.py
@@ -1,3 +1,17 @@
+from pickle import TRUE
 from django.db import models
 
 # Create your models here.
+
+class Personal(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    legajo = models.IntegerField(unique=TRUE)
+
+class Especialidad(models.Model):
+    especialidad = models.CharField(max_length=30)
+    esp_activa = models.BooleanField()
+
+class EquipoTrabajo(models.Model):
+    equipo = models.CharField(max_length=30)
+    responsable = models.CharField(max_length=30)
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ProyectoFinal
 Proyecto Final llevado a cabo con Django.
+
+El objetivo es generar una base de datos con los tecnicos de mantenimiento.
+Se crean 3 modelos: Personas, Especialidad y Equipo de trabajo
+Legajo (dentro de personas) es un identificador unico para cada una de las personas.
+
+
