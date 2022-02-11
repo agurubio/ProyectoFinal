611a9730be5791ca9f4f69cf00f7e4d86c22f836
Agustin Rubio <agustin.rubio@gmail.com>
2022-02-11 16:47:28 -0300

    busqueda y creacion en BD

--- Final/AppFinal/urls.py
+++ Final/AppFinal/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', views.inicio, name="Inicio" ),
     path('personal/', views.personal, name= "Personal" ),
     path('especialidad/', views.especialidad, name = "Especialidad" ),
-    path('equipoTrabajo/', views.equipo_trabajo, name = "Equipo_Trabajo" ),
-
-]
+    path('equipoTrabajo/', views.equipo_trabajo, name = "EquipoTrabajo" ),
+    path('personalFormulario/', views.personalFormulario, name= "PersonalFormulario" ),
+    path('busquedaPersonal/', views.busquedaPersonal, name= "BusquedaPersonal" ),
+    path('buscar/', views.buscar, name= "Buscar" ),
+    ]
